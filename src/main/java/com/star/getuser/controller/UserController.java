15b22c49b8206ac470b39ec14c61fb34195b123c
木三 <410296015@qq.com>
2023-07-28 08:22:50 +0800

    up
    up

--- src/main/java/com/star/getuser/controller/UserController.java
+++ src/main/java/com/star/getuser/controller/UserController.java
@@ -5,12 +5,13 @@ import com.star.getuser.common.Result;
 import com.star.getuser.entity.User;
 import com.star.getuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.List;
 
-@RestController
+@Controller
 @CrossOrigin
 @RequestMapping("/user")
 public class UserController {
@@ -20,25 +21,29 @@ public class UserController {
 
     @GetMapping
     public String getUser(){
-        return "user.html";
+        return "user";
     }
 
     @GetMapping("/getAll")
+    @ResponseBody
     public List<User> getAll(){
         return userService.getAll();
     }
 
     @GetMapping("/getCreateTime/{timeRange}")
+    @ResponseBody
     public Result getCreateTime(@PathVariable String timeRange) throws JsonProcessingException {
         return Result.success(userService.getCreateTime(timeRange));
     }
 
     @GetMapping("/compare")
+    @ResponseBody
     public Result compare() throws ParseException {
         return Result.success(userService.compare("month"));
     }
 
     @GetMapping("/getSource")
+    @ResponseBody
     public Result getSource() throws ParseException {
         return Result.success(userService.getSource());
     }
